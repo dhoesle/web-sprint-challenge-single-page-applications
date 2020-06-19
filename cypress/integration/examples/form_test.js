3f0b16b7c68b51a57bd6d853c890db5eeee2073c
Daniel Hoesle <d1@PacBook-Mro.local>
2020-06-19 11:37:14 -0600

    completed tests

--- cypress/integration/examples/form_test.js
+++ cypress/integration/examples/form_test.js
@@ -1,6 +1,6 @@
 describe('Inputs and Submit button', () => {
     it('can navigate to the site', () => {
-        cy.visit('http://localhost:3002')
+        cy.visit('http://localhost:3000')
         cy.url().should('include', 'localhost')
     })
     it('can type a text for a new name', () => {
@@ -13,11 +13,6 @@ describe('Inputs and Submit button', () => {
           .type('make with extra love')
           .should('have.value', 'make with extra love')
       })
-      it('can select size', () => {
-          cy.get('select')
-          .select(['Large', 'Medium', 'Small'])
-          .should('deep.equal',(['Large', 'Medium', 'Small']))
-        })
         it('can check boxes', () => {
           cy.get('[type="checkbox"]')
             .check()
